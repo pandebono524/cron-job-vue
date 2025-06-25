480c8e7fbe3fb1a48031a36c7c90bbf075cfedd9
pandebono524 <pandebono1012@gmail.com>
2025-06-25 03:02:28 +0100

    Initialize frontend project with Vite, Vue, and Tailwind CSS. Added essential configuration files, including package.json, vite.config.js, and tailwind.config.js. Created initial application structure with App.vue, main.js, and style.css. Added .gitignore to exclude unnecessary files and directories.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container mx-auto max-w-[1440px] bg-white">
+    <div class="flex flex-row w-full h-full min-h-screen relative">
+      <div class="fixed md:sticky top-0 h-screen z-20">
+        <Sidebar />
+      </div>
+      <main class="flex flex-col w-full h-auto min-h-screen overflow-x-auto ml-15 md:ml-0">
+        <Header></Header>
+        <TrendingSearches></TrendingSearches>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+import Header from './components/Header.vue';
+import TrendingSearches from './pages/TrendingSearches.vue';
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                ibm: ['"IBM Plex Sans"', 'sans-serif'],
+            },
+        },
+    },
+}
